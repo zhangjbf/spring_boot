59d8d47ce75e0462bdf80f4fe13de963657c797e
zhangjbf <javaboyok@163.com>
2018-08-09 10:27:18 +0800

    2342

---
+++ liantouApp/src/main/java/com/yonyou/app/LiantouAppApplication.java
@@ -0,0 +1,12 @@
+package com.yonyou.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiantouAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiantouAppApplication.class, args);
+	}
+}
