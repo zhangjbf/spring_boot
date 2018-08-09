b1ee42fbc78e1ca467955ef82de6daf404d2d82f
zhangjbf <javaboyok@163.com>
2018-08-09 12:07:31 +0800

    2342

--- liantouApp/src/main/java/com/yonyou/app/LiantouApplication.java
+++ liantouApp/src/main/java/com/yonyou/app/LiantouApplication.java
@@ -2,9 +2,17 @@ package com.yonyou.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
-public class LiantouApplication {
+public class LiantouApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(LiantouApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LiantouApplication.class, args);
